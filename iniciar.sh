8ec1488df8f7d2c1385e55c4d6ea87c05cf40e03
Luis Cabrera Benito <parzibyte@gmail.com>
2021-08-12 16:42:33 -0500

    Logueando

--- iniciar.sh
+++ iniciar.sh
@@ -2,5 +2,5 @@
 # Recuerda que antes debes compilar el programa
 # /usr/local/go/bin/go build servidor.go
 ~/control_arcade/servidor >> ~/control_arcade/servidor.log 2>&1 &
-/usr/bin/python3 ~/control_arcade/contador_tiempo.py ~/control_arcade/contador.log 2>&1 &
+/usr/bin/python3 ~/control_arcade/contador_tiempo.py >> ~/control_arcade/contador.log 2>&1 &
 /usr/bin/lxterminal -e /usr/bin/emulationstation
