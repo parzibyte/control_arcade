14f9a45ce0309629a2f376a2c8d834d770a11d48
Luis Cabrera Benito <parzibyte@gmail.com>
2021-08-12 14:26:31 -0500

    Rutas absolutas

--- iniciar.sh
+++ iniciar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Recuerda que antes debes compilar el programa
 # /usr/local/go/bin/go build servidor.go
-./servidor 2>1 &
-/usr/bin/python3 contador_tiempo.py 2>1 &
+~/control_arcade/servidor 2>1 &
+/usr/bin/python3 ~/control_arcade/contador_tiempo.py 2>1 &
 /usr/bin/lxterminal -e /usr/bin/emulationstation
